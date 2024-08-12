b78213460bec3e3964eb59e55c65ce462d203c04
Tebogo Mphahlele <119810900+TebzaDee@users.noreply.github.com>
2024-08-12 17:12:25 +0200

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assigmentqu2test;
+
+import tut.ac.za.SecureMessageFrame;
+
+/**
+ *
+ * @author IC
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       new SecureMessageFrame();
+    }
+    
+}
+
+
