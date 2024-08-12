468a1f859432a895878e554ac66cf05164220e98
Tebogo Mphahlele <119810900+TebzaDee@users.noreply.github.com>
2024-08-12 17:06:12 +0200

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut;
+
+
+public class Message 
+{
+    private String messageContents;
+
+    public Message(String messageContents) 
+    {
+        this.messageContents = messageContents;
+    }
+
+    public String getMessageContents() 
+    {
+        return messageContents;
+    }
+
+    public void setMessageContents(String messageContents)
+    {
+        this.messageContents = messageContents;
+    }
+    
+    
+}
